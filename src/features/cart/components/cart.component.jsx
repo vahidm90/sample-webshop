76749b077d6431220980919bd8d62929f519616f
VahidM <49893905+vahidm90@users.noreply.github.com>
2022-10-02 21:00:31 +0200

    add price to products; calculate cart items' total price;

--- src/features/cart/components/cart.component.jsx
+++ src/features/cart/components/cart.component.jsx
@@ -1,12 +1,23 @@
 import CartItem from "./cart-item.component";
 
 function Cart({items, onItemCountChange, onItemRemove}) {
-    return items.length ? items.map(item =>
-        <CartItem key={item.id} item={item}
-                  onIncrement={onItemCountChange.bind(undefined, 'increment', item)}
-                  onDecrement={onItemCountChange.bind(undefined, 'decrement', item)}
-                  onRemove={onItemRemove.bind(undefined, 'remove', item)}
-        />) : <p>Your shopping cart is empty!</p>;
+    const emptyCart = items.length ? '' : <p>Your shopping cart is empty!</p>;
+    let totalPrice = 0;
+    return (
+        <div className="flex-container fd-column">
+            {emptyCart}{
+            items.map(item => {
+                totalPrice += item.price * item.count;
+                return <CartItem key={item.id} item={item}
+                          onIncrement={onItemCountChange.bind(undefined, 'increment', item)}
+                          onDecrement={onItemCountChange.bind(undefined, 'decrement', item)}
+                          onRemove={onItemRemove.bind(undefined, 'remove', item)}
+                />;
+            })
+        }
+            <p>Total Price: {totalPrice}</p>
+        </div>
+    );
 }
 
 export default Cart;
